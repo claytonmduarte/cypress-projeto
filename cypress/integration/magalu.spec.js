277f34159383df1a7fbb2ba319e879a4b4e1fcbb
claytonmduarte <claytonmduarte@gmail.com>
2022-05-17 22:21:30 -0300

    Teste1

--- cypress/integration/magalu.spec.js
+++ cypress/integration/magalu.spec.js
@@ -2,9 +2,8 @@ describe(`Acessos em e-commerce `, () => {
     
     it('Acesso ao site netshoes', ()=>{
         cy.visit('https://www.netshoes.com.br')
+        
     })
-
-    it('Acesso ao site Kabum', ()=>{
-        cy.visit('https://www.kabum.com.br')
-    })
+    
+   
 });
