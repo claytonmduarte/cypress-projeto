12b9f853e6c50e69fc13571e2b678541877a5055
claytonmduarte <claytonmduarte@gmail.com>
2022-05-18 12:18:58 -0300

    Inclusão no carrinho e Validação

--- cypress/integration/magalu.spec.js
+++ cypress/integration/magalu.spec.js
@@ -2,20 +2,16 @@ describe(`Acesso ao site Magalu pesquisando um item e incluindo no carrinho`, ()
     it('Teste Carrinho', () =>{
         cy.viewport(1200, 850)
         cy.visit('https://www.magazineluiza.com.br/') 
-        cy.wait(2000)
         cy.get('[data-testid="button-message-box"]').click()
-        cy.wait(2000)
         cy.url().should('contain', 'magazineluiza') 
         cy.contains('Procure no magalu').should('be.visible') 
         cy.get('[class="sc-jWaEpP fXqluz"]').should('be.visible').type("hd externo") 
-        cy.wait(2000)
         cy.get('[data-testid="search-submit"]').click() 
-        cy.wait(2000)
         cy.contains("HD Externo 1TB ADATA AHV620S-1TU31-CBK").should('be.visible') 
-        cy.wait(2000)
         cy.contains("HD Externo 1TB ADATA AHV620S-1TU31-CBK").should('be.visible').click()
-        cy.wait(2000)
         cy.contains('Adicionar à sacola').should('be.visible').click()
-        cy.wait(2000)
+        cy.get('.price-warranty__btn--continue').click()
+        cy.contains("HD Externo 1TB ADATA AHV620S-1TU31-CBK - USB 3.1 ").should('be.visible') 
+        
     });
 });
